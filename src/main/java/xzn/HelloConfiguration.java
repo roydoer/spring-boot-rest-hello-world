f08ef2118bc2f804e6b19a62e2b45bc514b96b1b
Radu C <rchiriac@adobe.com>
2016-07-24 00:48:35 +0300

    Profile selection with --spring.profile.active param

--- src/main/java/xzn/HelloConfiguration.java
+++ src/main/java/xzn/HelloConfiguration.java
@@ -6,21 +6,13 @@ package xzn;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class HelloConfiguration {
 
   @Bean
-  @Profile("prod")
   public Prompt prompt(@Value("${prompt.content}") String promptMessage) {
     return new Prompt(promptMessage);
-  }
-
-  @Bean
-  @Profile("dev")
-  public Prompt promptDev() {
-    return new Prompt(">>DEV>>");
   }
 
 }
